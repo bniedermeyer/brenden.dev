11c3e416bb08ba8c80f77c2a4ab52b566798b9af
Brenden <brenden.niedermeyer@gmail.com>
2019-04-14 15:45:45 -0700

    feat: update routing and deploy script

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
-];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [
